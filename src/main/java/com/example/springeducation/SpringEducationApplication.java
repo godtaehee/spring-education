1e418df82fbd9b00341855360ac8e8874a9709bb
taehee <wonderfulhuman@naver.com>
2021-09-23 22:35:45 +0900

    Initial Commit

---
+++ src/main/java/com/example/springeducation/SpringEducationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springeducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEducationApplication.class, args);
+    }
+
+}
